e411c92fc15f799328205c48876189ddf53a1f99
RAFAEL FELIZ <rafael.feliz@univille.br>
2025-03-26 20:29:04 -0300

    alteração

---
+++ revisao_rafa/src/main/java/br/univille/controller/ControllerPessoa.java
@@ -0,0 +1,19 @@
+package br.univille.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
+public class ControllerPessoa implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(((JButton)e.getSource()).getName().equals("btnSair")){
+            System.exit(0);
+        }; //converte a ação do clique e um botão
+        JOptionPane.showMessageDialog(null,"EU NÃO ACREDITO!!!");
+    }
+
+}
