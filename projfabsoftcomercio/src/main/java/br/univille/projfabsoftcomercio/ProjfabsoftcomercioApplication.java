56ab1fef62dc8098670a1b9239e0be5c092586a9
RAFAEL FELIZ <rafael.feliz@univille.br>
2025-04-01 22:00:59 -0300

    alterações

---
+++ projfabsoftcomercio/src/main/java/br/univille/projfabsoftcomercio/ProjfabsoftcomercioApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoftcomercio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjfabsoftcomercioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjfabsoftcomercioApplication.class, args);
+	}
+
+}
---
+++ projfabsoftcomercio/src/test/java/br/univille/projfabsoftcomercio/ProjfabsoftcomercioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoftcomercio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjfabsoftcomercioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
