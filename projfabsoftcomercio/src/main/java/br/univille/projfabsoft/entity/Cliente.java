2e80f3f8ea1db77e6a7a235af13870cb0367ee2a
LUCAS ARINS CAMPOS <lucas.arins.campos@univille.br>
2025-04-08 21:44:25 -0300

    breanch: add @

--- projfabsoftcomercio/src/main/java/br/univille/projfabsoft/entity/Cliente.java
+++ projfabsoftcomercio/src/main/java/br/univille/projfabsoft/entity/Cliente.java
@@ -1,10 +1,15 @@
 package br.univille.projfabsoft.entity;
 
 import java.util.List;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     public Long getId() {
         return id;
     }
