59f32e1e65bcdfff4b567a9be64a49cac00af9b8
Lucas Arins <96632234+LucasArins@users.noreply.github.com>
2025-06-25 00:39:11 +0000

    criado form-cliente

---
+++ projfabsoft_frontend/src/app/form-adminstrador/form-adminstrador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-adminstrador',
+  imports: [],
+  templateUrl: './form-adminstrador.component.html',
+  styleUrl: './form-adminstrador.component.css'
+})
+export class FormAdminstradorComponent {
+
+}
